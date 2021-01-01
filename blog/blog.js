c95d2a0db4599dcddff4ea90c8a3d304d2e0fa47
Filip Kin <filip@kinmails.com>
2021-01-01 17:49:31 -0500

    blog

---
+++ blog/blog.js
@@ -0,0 +1,45 @@
+const API_ROOT = 'https://api.benzenenbots.com';
+
+const fetchPosts = (s = 0, n = 20) => {
+    fetch(API_ROOT + '/posts/' + n + '/' + s)
+        .then((res) => res.json())
+        .then((posts) => {
+            let out = document.getElementById('out');
+            for (let post of posts.posts) {
+                out.appendChild(generatePost(post));
+            }
+        });
+};
+
+const fetchPost = (id) => {
+    fetch(API_ROOT + '/post/' + id)
+        .then((res) => res.json())
+        .then((post) => {
+            let out = document.getElementById('out');
+            out.appendChild(generatePost(post.post, true));
+        });
+}
+
+const generatePost = (post, standalone = false) => {
+    let elm = document.createElement('div');
+    elm.classList.add('post');
+    elm.id = post.id;
+
+    let h = document.createElement('h3');
+    h.innerHTML = (standalone) ? post.title : `<a href="/blog/post.html?id=${post.id}">${post.title}</a>`;
+    elm.appendChild(h);
+
+    let d = document.createElement('div');
+    d.classList.add('post-published-date');
+    // This will problem break because DST
+    // I hate time
+    d.innerHTML = moment(new Date(post.published)).utcOffset(-60 * 10).format('LLL');
+    elm.appendChild(d);
+
+    let b = document.createElement('div');
+    b.classList.add('post-body');
+    b.innerHTML = md(post.content);
+    elm.appendChild(b);
+
+    return elm;
+}
--- partials/loader.js
+++ partials/loader.js
@@ -1,12 +1,14 @@
+let pathname = window.location.pathname.split('/');
+delete pathname[pathname.length-1];
+pathname = pathname.join('/');
+
 const loadNav = () => {
-    let pathname = window.location.pathname.split('/');
-    delete pathname[pathname.length-1];
-    pathname = pathname.join('/');
-
     document.querySelector(`nav li a[href="${pathname}"]`).parentNode.classList.add('active');
-    document.querySelector(`.sidenav li a[href="${pathname}"]`).parentNode.classList.add('active');
-
     M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {hover: true, constrainWidth: false});
+};
+
+const loadMobileNav = () => {
+    document.querySelector(`.sidenav li a[href="${pathname}"]`).parentNode.classList.add('active');
     M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
 };
 
@@ -15,11 +17,25 @@ fetch('/partials/meta.html')
 .then((data) => document.head.innerHTML += data)
 .catch((err) => console.error(err));
 
+
 fetch('/partials/nav.html')
 .then((res) => res.text())
 .then((data) => {
-    document.body.innerHTML = data + document.body.innerHTML;
+    document.getElementById('nav').innerHTML = data;
     loadNav();
+    // Imagine writing good code
+    setTimeout(loadNav, 1e3);
+    setTimeout(loadNav, 3e3);
+})
+.catch((err) => console.error(err));
+
+fetch('/partials/mobile-nav.html')
+.then((res) => res.text())
+.then((data) => {
+    document.getElementById('mobile-nav').innerHTML = data;
+    loadMobileNav();
+    setTimeout(loadMobileNav, 1e3);
+    setTimeout(loadMobileNav, 3e3);
 })
 .catch((err) => console.error(err));
 
