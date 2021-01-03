0759bf388341683bd63a8b14567290049fd2bad2
Filip Kin <filip@kinmails.com>
2021-01-03 16:10:32 -0500

    skip login if already authorized

--- admin/login.js
+++ admin/login.js
@@ -1,9 +1,11 @@
+const API_ROOT = 'https://api.benzenebots.com';
+
 const login = async (password, email) => {
     let hashedPassword = await new Promise((resolve, reject) => crypto.subtle.digest('SHA-256', (new TextEncoder()).encode(password)).then(hash => {
         resolve(Array.prototype.map.call(new Uint8Array(hash), x => ('00' + x.toString(16)).slice(-2)).join(''));
     }));
 
-    fetch('https://api.benzenebots.com/user/login', {
+    fetch(API_ROOT + '/user/login', {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
@@ -16,8 +18,8 @@ const login = async (password, email) => {
         .then(res => res.json())
         .then(json => {
             if (json.user) {
-                localStorage.setItem("id", json.id);
-                localStorage.setItem("password", json.password);
+                localStorage.setItem("id", json.user.id);
+                localStorage.setItem("password", json.user.password);
                 window.location.href = "/admin/";
             } else {
                 document.getElementById("login-error").innerText = json.error;
@@ -38,6 +40,33 @@ const addSubmitListener = () => {
     document.forms[0].addEventListener('submit', (evt) => {
         evt.preventDefault();
     });
+    if (localStorage.id) {
+        fetch(API_ROOT + '/user/auth', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                id: localStorage.id,
+                password: localStorage.password
+            })
+        })
+        .then(res => res.json())
+        .then(json => {
+            if (json.user) {
+                window.location.href = "/admin/";
+            } else {
+                document.getElementById("login-error").innerText = json.error;
+                localStorage.removeItem('id');
+                localStorage.removeItem('password');
+            }
+        })
+        .catch(err => {
+            M.toast({
+                text: err.message
+            });
+        });
+    }
 }
 
 addSubmitListener();
