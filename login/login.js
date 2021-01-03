5a8ad40b8c38f9d9d3fe3ea22afcdd29dbda63ec
Ash <ashp116@outlook.com>
2021-01-03 14:00:28 -0500

    Got login done

---
+++ login/login.js
@@ -0,0 +1,30 @@
+function login() {
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    return crypto.subtle.digest('SHA-256', (new TextEncoder()).encode(password)).then(hash => {
+        let hashedPassword = Array.prototype.map.call(new Uint8Array(hash), x => ('00' + x.toString(16)).slice(-2)).join('');
+        fetch('https://api.benzenebots.com/user/login', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({email:email,password:hashedPassword})
+        }).then(res => res.json()).then(json => {
+                if (json.user) {
+                    localStorage.setItem("email",email)
+                    localStorage.setItem("password",hashedPassword)
+                    console.log(localStorage.getItem("email"),localStorage.getItem("password"))
+                    window.location.href = "/adminPanel/";
+                }
+                else {
+                    document.getElementById("login-error").innerText = json.error;
+                    setTimeout(function () {
+                        document.getElementById("login-error").innerText = "";
+                    },5000)
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    })
+}
