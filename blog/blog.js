2a769632727b74f1d335301e8475a15fb8828c8a
Ash <ashp116@outlook.com>
2021-01-04 17:29:39 -0500

    Added a user panel

--- blog/blog.js
+++ blog/blog.js
@@ -1,3 +1,5 @@
+const API_ROOT = 'https://api.benzenebots.com';
+
 const blogList = (admin = false, s = 0, e = 20) => {
     fetch(API_ROOT + '/posts/' + s + '/' + e)
         .then((res) => res.json())
@@ -6,20 +8,6 @@ const blogList = (admin = false, s = 0, e = 20) => {
             for (let post of posts.posts) {
                 out.appendChild(generatePost(post, false, admin));
             }
-        });
-};
-
-const blogListSimple = (admin = false, s = 0, e = 20) => {
-    fetch(API_ROOT + '/posts/' + s + '/' + e)
-        .then((res) => res.json())
-        .then((posts) => {
-            let out = document.getElementById('out');
-            for (let post of posts.posts) {
-                out.appendChild(generateSimplePost(post, admin));
-            }
-            document.querySelectorAll('.delete-button').forEach((btn) => {
-                btn.addEventListener('click', () => modals[0].open());
-            });
         });
 };
 
@@ -44,33 +32,11 @@ const blogHomepage = () => {
             if (posts.posts.length < 4) {
                 let push = document.createElement('div');
                 push.classList.add('col');
-                push.classList.add('m' + (3 * (4 - posts.posts.length)));
+                push.classList.add('m'+(3*(4-posts.posts.length)));
                 push.classList.add('hide-on-small-only');
                 out.appendChild(push);
             }
         });
-}
-
-const generateSimplePost = (post, admin = false) => {
-    let elm = document.createElement('tr');
-    elm.id = post.id;
-
-    let h = document.createElement('td');
-    h.innerHTML = post.title;
-    elm.appendChild(h);
-
-    let d = document.createElement('td');
-    d.innerHTML = moment(new Date(post.published)).utcOffset(-60*5).format('LLL');
-    elm.appendChild(d);
-
-    let b = document.createElement('td');
-    if (admin) b.innerHTML += `
-    <a href="/blog/post.html?id=${post.id}" class="btn red darken-3">View</a>
-    <a href="/admin/blog/edit.html?id=${post.id}" class="btn red darken-3">Edit</a>
-    <a href="#!${post.id}" class="btn red darken-3 delete-button">Delete</a>`;
-    elm.appendChild(b);
-
-    return elm;
 }
 
 const generatePost = (post, standalone = false, admin = false) => {
@@ -84,17 +50,15 @@ const generatePost = (post, standalone = false, admin = false) => {
 
     let d = document.createElement('div');
     d.classList.add('post-published-date');
-    d.innerHTML = moment(new Date(post.published)).utcOffset(-60*5).format('LLL');
+    d.innerHTML = moment(new Date(post.published)).utcOffset(0).format('LLL');
     elm.appendChild(d);
 
     let b = document.createElement('div');
     b.classList.add('post-body');
-    b.innerHTML = md(post.content, {
-        breaks: true
-    });
+    b.innerHTML = md(post.content, {breaks: true});
     if (admin) b.innerHTML += `<div class="row post-controls">
-    <a href="/admin/blog/edit.html?id=${post.id}" class="btn red darken-3">Edit</a>
-    <a href="/admin/blog/delete.html?id=${post.id}" class="btn red darken-3">Delete</a>
+    <a href="/admin/editPost.html?id=${post.id}" class="btn red darken-3">Edit</a>
+    <a href="/admin/deletePost.html?id=${post.id}" class="btn red darken-3">Delete</a>
     </div>`;
     elm.appendChild(b);
 
@@ -103,7 +67,7 @@ const generatePost = (post, standalone = false, admin = false) => {
 
 const cleanUpContent = (content) => {
     let list = content.replaceAll(/#.+\n/g, '').replaceAll(/!\[.+\]\(.+\)/g, '').split('\n');
-    if (list[0].length < 120 && list.length > 1) return list[0] + list[1];
+    if (list[0].length < 120 && list.length > 1) return list[0]+list[1];
     return list[0];
 }
 
@@ -125,7 +89,7 @@ const generateCard = (post) => {
     card.appendChild(cardImg);
 
     let img = document.createElement('img');
-    img.src = (post.images.length > 0) ? 'https://api.benzenebots.com/' + post.images[0] : '/img/img2.webp';
+    img.src = (post.images.length > 0) ? 'https://api.benzenebots.com/'+post.images[0] : '/img/img2.webp';
     cardImg.appendChild(img);
 
     let h = document.createElement('span');
@@ -136,7 +100,7 @@ const generateCard = (post) => {
 
     let cardContent = document.createElement('div');
     cardContent.classList.add('card-content');
-    cardContent.innerHTML = `<div style="font-size: 10pt; margin-bottom: 10px">Published ${moment(new Date(post.published)).utcOffset(-60*5).format('LL')}</div>\n${md(cleanUpContent(post.content))}`;
+    cardContent.innerHTML = `<div style="font-size: 10pt; margin-bottom: 10px">Published ${moment(new Date(post.published)).utcOffset(0).format('LL')}</div>\n${md(cleanUpContent(post.content))}`;
     card.appendChild(cardContent);
 
     let action = document.createElement('div');
