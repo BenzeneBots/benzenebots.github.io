9d16cc9143f246c2dd195309956b81aa6d5717b7
Filip Kin <filip@kinmails.com>
2021-01-15 00:15:56 -0500

    yay

--- admin/users/users.js
+++ admin/users/users.js
@@ -28,3 +28,28 @@ const getAllUsers = async () => {
         });
     }
 };
+
+const getUser = async () => {
+    let json = await fetch(API_ROOT + '/user/' + USER_ID, {
+            method: 'GET',
+            headers: {
+                id: auth.id,
+                password: auth.password
+            }
+        })
+        .then(res => res.json());
+
+    if (json.user) {
+        document.getElementById('name').value = json.user.name;
+        document.getElementById('email').value = json.user.email;
+        for (let i of document.getElementById('role').children) {
+            if (i.value === json.user.role) i.selected = 'true';
+            else i.selected = 'false';
+        }
+        setTimeout(() => M.FormSelect.init(document.getElementById('role'), {}), 3e3);
+    } else {
+        M.toast({
+            html: json.error
+        });
+    }
+}
