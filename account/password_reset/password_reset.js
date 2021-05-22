0f9e9012a5cf6562933f15a5b1b21776dcb29b59
Ash <ashp116@outlook.com>
2021-05-21 21:07:05 -0400

    Reset Password

--- account/password_reset/password_reset.js
+++ account/password_reset/password_reset.js
@@ -1,10 +1,40 @@
+let params = {}
+
 function confirmPass()  {
     if (document.getElementById("confirm_password").value !== document.getElementById("new_password").value) {
         document.getElementById("password-error").innerText = "Passwords don't match"
+        return false
     }
     else {
         document.getElementById("password-error").innerText = ""
+        return true
     }
+}
+
+function checkExpire() {
+    let search = window.location.search.toString().replace("?","").split("&")
+
+    search.forEach((value, index) => {
+        if (value.toString().split("=")) {
+            let rawParams = value.toString().split("=")
+            params[rawParams[0]] = rawParams[1]
+        }
+    })
+
+    fetch(API_ROOT + `/temp/${params.sessionId}/expiration`, {
+        method: "GET",
+        headers: {
+            id: localStorage.id,
+            password: localStorage.password
+        }
+    })
+        .then(res => res.json())
+        .then(json => {
+            console.log(json)
+            if (!json.expired || json.error) {
+                window.location = "localhost:232"
+            }
+        })
 }
 
 const addSubmitListener = () => {
@@ -13,8 +43,49 @@ const addSubmitListener = () => {
     });
 }
 
-function handleFormSubmit() {
-    console.log("Test")
+addSubmitListener();
+setTimeout(addSubmitListener, 1e3);
+setTimeout(addSubmitListener, 3e3);
+
+async function handleFormSubmit() {
+    let proceed = confirmPass()
+
+    if (proceed) {
+        let password = document.getElementById("confirm_password").value
+        let hashedPassword = await new Promise((resolve, reject) => crypto.subtle.digest('SHA-256', (new TextEncoder()).encode(password)).then(hash => {
+            resolve(Array.prototype.map.call(new Uint8Array(hash), x => ('00' + x.toString(16)).slice(-2)).join(''));
+        }));
+
+        let data = {
+            "id": params.sessionId,
+            "password": hashedPassword,
+            "userId": params.userId
+        }
+
+        fetch(API_ROOT + `/user/${params.userId}/resetpassword`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                id: localStorage.id,
+                password: localStorage.password
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(json => {
+
+                if (json.success) {
+                    window.location = "/admin/"
+                }
+                else {
+                    window.location = "/"
+                }
+            })
+            .catch(err => {
+                M.toast({
+                    text: err.message
+                });
+            });
+    }
 }
-addSubmitListener()
 
