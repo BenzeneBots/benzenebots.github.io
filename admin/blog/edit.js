0cbfd841a776e0aa98d26ee13ef03a2cae7b3810
Ash <ashp116@outlook.com>
2022-02-17 22:46:17 -0500

    Blog System

--- admin/blog/edit.js
+++ admin/blog/edit.js
@@ -11,3 +11,213 @@ let post = {
     images: []
 };
 let postModified = post;
+
+// TinyMCE functions
+function editorRefresh() {
+    let editorContent = tinymce.activeEditor.getContent();
+    document.getElementById('preview').innerHTML = editorContent
+    localStorage.setItem("blog_draft",editorContent)
+}
+
+function editorInit() {
+    if (localStorage.getItem("blog_draft") !== null) tinymce.activeEditor.setContent(localStorage.getItem("blog_draft"));
+    document.getElementById('preview').innerHTML = localStorage.getItem("blog_draft")
+   editorRefresh();
+}
+
+let configTinyMCE = {
+    selector: 'textarea',
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table paste imagetools wordcount',
+        'codesample emoticons media wordcount slashcommands'
+    ],
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media| emoticons codesample wordcount | gallery',
+    editor: "restore_draft",
+    toolbar_mode: 'floating',
+    automatic_uploads: true,
+    image_title: true,
+    file_picker_types: 'image',
+    file_picker_callback: function (cb, value, meta) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+
+        input.onchange = function () {
+            var file = this.files[0];
+
+            var reader = new FileReader();
+            reader.onload = async function () {
+                const formData = new FormData()
+                formData.append('image', file)
+
+                let uploadData = await fetch('https://api.benzenebots.com/store/upload', {
+                    method: 'POST',
+                    "headers": {
+                        "password": "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",
+                        "id": "a34de56a-bb3a-483a-8d96-e95f885b3396",
+                    },
+                    body: formData
+                })
+                let uploadJson = await uploadData.json();
+
+                let galleryData = await fetch('https://api.benzenebots.com/gallery', {
+                    method: 'POST',
+                    "headers": {
+                        "password": "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",
+                        "id": "a34de56a-bb3a-483a-8d96-e95f885b3396",
+                    },
+                    body: formData
+                })
+                let galleryJson = await galleryData.json();
+
+                cb("https://api.benzenebots.com/"+uploadJson.id, { title: file.name });
+            };
+
+
+            reader.readAsDataURL(file);
+        };
+
+        input.click();
+    },
+    setup : function(editor) {
+        editor.on('init', editorInit);
+        editor.on('Paste Change input Undo Redo', function(e) {
+            editorRefresh()
+        });
+        editor.ui.registry.addButton('gallery', {
+            icon : `gallery`,
+            onAction: function (_) {
+                tinyMCE.activeEditor.windowManager.open({
+                    title: 'My Gallery', // The dialog's title - displayed in the dialog header
+                    size: 'large',
+                    body: {
+                        type: 'panel', // The root body type - a Panel or TabPanel
+                        items: [ // A list of panel components
+                            {
+                                type: 'htmlpanel', // A HTML panel component
+                                html: '<div class="gallery-div"><input type="hidden" id="image"/>Your Gallery Items Html</div>'
+                            }
+                        ]
+                    },
+                    buttons: [
+                        {
+                            type: 'cancel',
+                            name: 'closeButton',
+                            text: 'Cancel'
+                        },
+                        {
+                            type: 'submit',
+                            name: 'submitButton',
+                            text: 'Insert To Editor',
+                            primary: true
+                        }
+                    ],
+                    onSubmit: function (api) {
+
+                    }
+                });
+            }
+        });
+    },
+    image_caption: true,
+    branding: false,
+    skin: "oxide-dark",
+    icons: "thin",
+    content_css: "dark",
+}
+tinymce.PluginManager.add('slashcommands', function (editor) {
+    var insertActions = [
+        {
+            text: 'Heading 1',
+            icon: 'h1',
+            action: function () {
+                editor.execCommand('mceInsertContent', false, '<h1>Heading 1</h1>')
+                editor.selection.select(editor.selection.getNode());
+            }
+        },
+        {
+            text: 'Heading 2',
+            icon: 'h2',
+            action: function () {
+                editor.execCommand('mceInsertContent', false, '<h2>Heading 2</h2>');
+                editor.selection.select(editor.selection.getNode());
+            }
+        },
+        {
+            text: 'Heading 3',
+            icon: 'h3',
+            action: function () {
+                editor.execCommand('mceInsertContent', false, '<h3>Heading 3</h3>');
+                editor.selection.select(editor.selection.getNode());
+            }
+        },
+        {
+            type: 'separator'
+        },
+        {
+            text: 'Paragraph',
+            icon: 'p',
+            action: function () {
+                editor.execCommand('mceInsertContent', false, '<p>Paragraph</p>');
+                editor.selection.select(editor.selection.getNode());
+            }
+        },
+        {
+            type: 'separator'
+        },
+        {
+            text: 'Bulleted list',
+            icon: 'unordered-list',
+            action: function () {
+                editor.execCommand('InsertUnorderedList', false);
+            }
+        },
+        {
+            text: 'Numbered list',
+            icon: 'ordered-list',
+            action: function () {
+                editor.execCommand('InsertOrderedList', false);
+            }
+        }
+    ];
+
+    // Register the slash commands autocompleter
+    editor.ui.registry.addAutocompleter('slashcommands', {
+        ch: '/',
+        minChars: 0,
+        columns: 1,
+        fetch: function (pattern) {
+            const matchedActions = insertActions.filter(function (action) {
+                return action.type === 'separator' ||
+                    action.text.toLowerCase().indexOf(pattern.toLowerCase()) !== -1;
+            });
+
+            return new tinymce.util.Promise(function (resolve) {
+                var results = matchedActions.map(function (action) {
+                    return {
+                        meta: action,
+                        text: action.text,
+                        icon: action.icon,
+                        value: action.text,
+                        type: action.type
+                    }
+                });
+                resolve(results);
+            });
+        },
+        onAction: function (autocompleteApi, rng, action, meta) {
+            editor.selection.setRng(rng);
+            // Some actions don't delete the "slash", so we delete all the slash
+            // command content before performing the action
+            editor.execCommand('Delete');
+            meta.action();
+            autocompleteApi.hide();
+        }
+    });
+    return {};
+});
+
+tinyMCE.remove('textarea')
+tinyMCE.init(configTinyMCE);
--- blog/blog.js
+++ blog/blog.js
@@ -1,149 +1,29 @@
-const blogList = (admin = false, s = 0, e = 20) => {
-    fetch(API_ROOT + '/posts/' + s + '/' + e)
-        .then((res) => res.json())
-        .then((posts) => {
-            let out = document.getElementById('out');
-            for (let post of posts.posts) {
-                out.appendChild(generatePost(post, false, admin));
-            }
-        });
+const blogList = async (admin = false, s = 0, e = 20) => {
+    let data = await fetch('https://api.benzenebots.com/posts/' + s + '/' + e)
+    return await data.json();
 };
 
-const blogListSimple = (admin = false, s = 0, e = 20) => {
-    fetch(API_ROOT + '/posts/' + s + '/' + e)
-        .then((res) => res.json())
-        .then((posts) => {
-            let out = document.getElementById('out');
-            for (let post of posts.posts) {
-                out.appendChild(generateSimplePost(post, admin));
-            }
-        });
-};
 
-const blogStandalone = (id) => {
-    fetch(API_ROOT + '/post/' + id)
-        .then((res) => res.json())
-        .then((post) => {
-            let out = document.getElementById('out');
-            out.appendChild(generatePost(post.post, true));
-        });
+const generateCards = async () => {
+    let posts = await blogList(true, 0, 100)
+    posts = posts.posts;
+
+    posts.forEach((value, index) => {
+        let id = value.id;
+        let title = value.title;
+        let category = value.category;
+
+        document.getElementById("blog-body").innerHTML += `
+                <tr>
+                    <td style="width: 100%;">
+                        <span>
+                            <a class="blog-list-link" href="/blog/post.html?id=${id}">${title}</a>
+                        </span>
+                    </td>
+                    <td>${category}</td>
+                </tr> 
+`
+    })
 }
-
-const blogHomepage = () => {
-    fetch(API_ROOT + '/posts/0/4')
-        .then((res) => res.json())
-        .then((posts) => {
-            let out = document.getElementById('out');
-            for (let post of posts.posts) {
-                post.images = JSON.parse(post.images);
-                out.appendChild(generateCard(post));
-            }
-            if (posts.posts.length < 4) {
-                let push = document.createElement('div');
-                push.classList.add('col');
-                push.classList.add('m'+(3*(4-posts.posts.length)));
-                push.classList.add('hide-on-small-only');
-                out.appendChild(push);
-            }
-        });
-}
-
-const generateSimplePost = (post, admin = false) => {
-    let elm = document.createElement('tr');
-    elm.id = post.id;
-
-    let h = document.createElement('td');
-    h.innerHTML = post.title;
-    elm.appendChild(h);
-
-    let d = document.createElement('td');
-    d.innerHTML = moment(new Date(post.published)).utcOffset(-60*5).format('LLL');
-    elm.appendChild(d);
-
-    let b = document.createElement('td');
-    if (admin) b.innerHTML += `
-    <a href="/blog/post.html?id=${post.id}" class="btn red darken-3">View</a>
-    <a href="/admin/blog/edit.html?id=${post.id}" class="btn red darken-3">Edit</a>
-    <a href="/admin/blog/delete.html?id=${post.id}" class="btn red darken-3">Delete</a>`;
-    elm.appendChild(b);
-
-    return elm;
-}
-
-const generatePost = (post, standalone = false, admin = false) => {
-    let elm = document.createElement('div');
-    elm.classList.add('post');
-    elm.id = post.id;
-
-    let h = document.createElement('h3');
-    h.innerHTML = (standalone) ? post.title : `<a href="/blog/post.html?id=${post.id}">${post.title}</a>`;
-    elm.appendChild(h);
-
-    let d = document.createElement('div');
-    d.classList.add('post-published-date');
-    d.innerHTML = moment(new Date(post.published)).utcOffset(0).format('LLL');
-    elm.appendChild(d);
-
-    let b = document.createElement('div');
-    b.classList.add('post-body');
-    b.innerHTML = md(post.content, {breaks: true});
-    if (admin) b.innerHTML += `<div class="row post-controls">
-    <a href="/admin/blog/edit.html?id=${post.id}" class="btn red darken-3">Edit</a>
-    <a href="/admin/blog/delete.html?id=${post.id}" class="btn red darken-3">Delete</a>
-    </div>`;
-    elm.appendChild(b);
-
-    return elm;
-}
-
-const cleanUpContent = (content) => {
-    let list = content.replaceAll(/#.+\n/g, '').replaceAll(/!\[.+\]\(.+\)/g, '').split('\n');
-    if (list[0].length < 120 && list.length > 1) return list[0]+list[1];
-    return list[0];
-}
-
-const generateCard = (post) => {
-    let elm = document.createElement('div');
-    elm.classList.add('col');
-    elm.classList.add('s6');
-    elm.classList.add('m3');
-    elm.id = post.id;
-
-    let card = document.createElement('div');
-    card.classList.add('card');
-    card.classList.add('grey');
-    card.classList.add('darken-4');
-    elm.appendChild(card);
-
-    let cardImg = document.createElement('div');
-    cardImg.classList.add('card-image');
-    card.appendChild(cardImg);
-
-    let img = document.createElement('img');
-    img.src = (post.images.length > 0) ? 'https://api.benzenebots.com/'+post.images[0] : '/img/img2.webp';
-    cardImg.appendChild(img);
-
-    let h = document.createElement('span');
-    h.classList.add('card-title');
-    h.classList.add('truncate');
-    h.innerHTML = post.title;
-    cardImg.appendChild(h);
-
-    let cardContent = document.createElement('div');
-    cardContent.classList.add('card-content');
-    cardContent.innerHTML = `<div style="font-size: 10pt; margin-bottom: 10px">Published ${moment(new Date(post.published)).utcOffset(0).format('LL')}</div>\n${md(cleanUpContent(post.content))}`;
-    card.appendChild(cardContent);
-
-    let action = document.createElement('div');
-    action.classList.add('card-action');
-    card.appendChild(action);
-
-    let a = document.createElement('a');
-    a.classList.add('red-text');
-    a.classList.add('text-darken-3');
-    a.innerHTML = 'Read More';
-    a.href = `/blog/post.html?id=${post.id}`;
-    action.appendChild(a);
-
-    return elm;
-}
+generateCards()
+/**/
